8c98eade8de86f97988480af8b775158e8540bbc
Malacalypse <malacalypse@users.noreply.github.com>
2020-07-02 04:53:22 +0000

    Start to get some knob UI action going on

--- h9_external.c
+++ h9_external.c
@@ -17,8 +17,13 @@ typedef struct _h9_external {
     t_object    ob;
     t_symbol    *name;
 
+    float knobs[NUM_CONTROLS];
+
+    void *m_outlet_dirty;
+    void *m_outlet_cc;
     void *m_outlet_sysex;
     void *m_outlet_enabled;
+    void *m_outlet_knobs;
 
     // libh9 object
     h9* h9;
@@ -35,6 +40,42 @@ void h9_external_identify(t_h9_external *x);
 void h9_external_set(t_h9_external *x, t_symbol *s, long ac, t_atom *av);
 void h9_external_int(t_h9_external *x, long n);
 void h9_external_list(t_h9_external *x, t_symbol *msg, long argc, t_atom *argv);
+
+// Callback handlers
+void output_cc(void *ctx, uint8_t midi_channel, uint8_t cc, uint8_t msb, uint8_t lsb) {
+    t_h9_external *x = (t_h9_external *)ctx;
+    t_atom list[2];
+    atom_setlong(&list[0], cc);
+    atom_setlong(&list[1], msb);
+    outlet_list(x->m_outlet_cc, gensym("cc"), 2, list);
+}
+
+void output_knobs(void *ctx, control_id control, float value) {
+    t_h9_external *x = (t_h9_external *)ctx;
+    x->knobs[control] = value;
+    t_atom list[NUM_CONTROLS];
+    for (size_t i = 0; i < NUM_CONTROLS; i++) {
+        atom_setfloat(&list[i], x->knobs[i]);
+    }
+    outlet_list(x->m_outlet_knobs, gensym("knobs"), NUM_CONTROLS, list);
+}
+
+void output_sysex(void *ctx, uint8_t *sysex, size_t len) {
+    t_h9_external *x = (t_h9_external *)ctx;
+    if (len > 0) {
+        // Big atom lists of sysex might be a bit large for the stack, so ask for heap
+        t_atom *list = malloc(sizeof(t_atom) * len);
+        if (list == NULL) {
+            object_post((t_object *)x, "Ran out of memory dumping sysex!");
+            return;
+        }
+        for (size_t i = 0; i < len; i++) {
+            atom_setlong(&list[i], sysex[i]);
+        }
+        outlet_list(x->m_outlet_sysex, gensym("sysex"), len, list);
+        free(list);
+    }
+}
 
 //////////////////////// global class pointer variable
 void *h9_external_class;
@@ -76,10 +117,18 @@ void *h9_external_new(t_symbol *s, long argc, t_atom *argv)
             x->name = symbol_unique();
 
         x->m_outlet_enabled = intout((t_object *)x);
+        x->m_outlet_dirty = intout((t_object *)x);
         x->m_outlet_sysex = listout((t_object *)x);
+        x->m_outlet_cc = listout((t_object *)x);
+        x->m_outlet_knobs = listout((t_object *)x);
 
         // Init the zero state of the object
         x->h9 = h9_new();
+        x->h9->cc_callback = output_cc;
+        x->h9->display_callback = output_knobs;
+        x->h9->sysex_callback = output_sysex;
+        x->h9->callback_context = x;
+
         if(x->h9 == NULL) {
             h9_external_free(x);
             object_free(x);
@@ -105,7 +154,16 @@ void h9_external_assist(t_h9_external *x, void *b, long m, long a, char *s)
 	else {	// outlet
         switch(a) {
             case 0:
-                sprintf(s, "I am outlet ZERO");
+                sprintf(s, "Knob list [0-9, exp, psw]");
+                break;
+            case 1:
+                sprintf(s, "CC output [cc, val]");
+                break;
+            case 2:
+                sprintf(s, "Sysex output as list");
+                break;
+            case 3:
+                sprintf(s, "Dirty state: 1 or 0");
                 break;
             default:
                 sprintf(s, "I am outlet %ld", a);
@@ -115,7 +173,7 @@ void h9_external_assist(t_h9_external *x, void *b, long m, long a, char *s)
 
 void h9_external_free(t_h9_external *x)
 {
-
+    h9_delete(x->h9);
 }
 
 // Input handlers for each message
@@ -127,26 +185,58 @@ void h9_external_int(t_h9_external *x, long n)
 
 void h9_external_list(t_h9_external *x, t_symbol *msg, long argc, t_atom *argv) {
     char list[argc];
+    float controls[NUM_CONTROLS];
     long i = 0;
-    for (; i < argc; i++) {
-        if (atom_gettype(&argv[i]) != A_LONG) {
-            object_post((t_object *)x, "List contains non-integer values, refusing to parse further.");
-            return;
-        }
-        long value = atom_getlong(&argv[i]);
-        if (value > UINT8_MAX) {
-            object_post((t_object *)x, "List does not contain character-value integers, refusing to parse further.");
-            return;
-        }
-        list[i] = (char)value;
-    }
-    object_post((t_object *)x, "Received list of %d characters.", i);
-    if (h9_load(x->h9, (uint8_t *)list, i) == kH9_OK) {
-        object_post((t_object *)x, "Successfully loaded preset %s.", x->h9->preset->name);
-    } else {
-        object_post((t_object *)x, "Not a preset, ignored.");
-    }
 
+    // Decide what to do by the first item in the list
+    switch(atom_gettype(argv)) {
+        case A_LONG:
+            // Scan the rest to make sure it's all longs <= UINT8_MAX, and treat as sysex
+            for (i = 0; i < argc; i++) {
+                if (atom_gettype(&argv[i]) != A_LONG) {
+                    object_post((t_object *)x, "List contains non-integer values, refusing to parse further.");
+                    return;
+                }
+                long value = atom_getlong(&argv[i]);
+                if (value > UINT8_MAX) {
+                    object_post((t_object *)x, "List does not contain character-value integers, refusing to parse further.");
+                    return;
+                }
+                list[i] = (char)value;
+                object_post((t_object *)x, "Received list of %d characters.", i);
+                if (h9_load(x->h9, (uint8_t *)list, i) == kH9_OK) {
+                    object_post((t_object *)x, "Successfully loaded preset %s.", x->h9->preset->name);
+                } else {
+                    object_post((t_object *)x, "Not a preset, ignored.");
+                }
+            }
+            break;
+        case A_FLOAT:
+            // It's a list of knob values, if all the floats are between 0. and 1.
+            if (argc != NUM_CONTROLS) {
+                object_post((t_object *)x, "Insufficient quantity of floats for controls, need exactly %d.", NUM_CONTROLS);
+                    return;
+            }
+            for (i = 0; i < argc; i++) {
+                if (atom_gettype(&argv[i]) != A_FLOAT) {
+                    object_post((t_object *)x, "List contains non-float values, refusing to parse further.");
+                    return;
+                }
+                long value = atom_getfloat(&argv[i]);
+                if (value > 1.0f || value < 0.0f) {
+                    object_post((t_object *)x, "Float values are out of range, cannot accept for knobs.");
+                    return;
+                }
+                controls[i] = value;
+            }
+            for (i = 0; i < NUM_CONTROLS; i++) {
+                h9_setControl(x->h9, (control_id)i, controls[i], kH9_TRIGGER_CALLBACK);
+            }
+            break;
+        default:
+            // No clue what it is, let's just ignore it
+            object_post((t_object *)x, "Input format not recognized, ignoring.");
+    }
 }
 
 void h9_external_set(t_h9_external *x, t_symbol *s, long argc, t_atom *argv)
@@ -185,14 +275,8 @@ void h9_external_bang(t_h9_external *x)
 {
     object_post((t_object *)x, "%s says \"Bang!\"", x->name->s_name);
     uint8_t sysex_buffer[1000];
-    t_atom list[1000];
     size_t bytes_written = h9_dump(x->h9, sysex_buffer, 1000, true);
-    if (bytes_written > 0) {
-        for (size_t i = 0; i < bytes_written; i++) {
-            atom_setlong(&list[i], sysex_buffer[i]);
-        }
-        outlet_list(x->m_outlet_sysex, gensym("sysex"), bytes_written, list);
-    }
+    output_sysex((void *)x, sysex_buffer, bytes_written);
 }
 
 void h9_external_identify(t_h9_external *x)
